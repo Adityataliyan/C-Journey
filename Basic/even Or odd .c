c8e7b18b07ee9804e43ce9e4ee18c92723b4552a
Aditya Taliyan <adityataliyan123@gmail.com>
2024-11-21 10:59:29 +0530

    Create even Or odd .c
    Code check no is Even or Odd in C

---
+++ Basic/even Or odd .c
@@ -0,0 +1,14 @@
+#include<stdio>
+#include<conio.h>
+  void mian()
+{
+  clrscr();
+  int A=0;
+  printf("Enter the Number");
+  scanf("%d",&A);
+  if (A%2==0)
+{printf("Even");}
+  else
+{printf("odd");}
+  getch();
+}
