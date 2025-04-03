42aab405010feaef91b98550aaa8980ef63c2db4
Aditya Taliyan <adityataliyan123@gmail.com>
2025-04-03 13:37:27 +0530

    Create User difined datatype.c
    In this program i use typedef  and make a program to area of square.

---
+++ Basic/User difined datatype.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  clrscr();
+  typedef int aditya;
+  aditya side;
+  printf("Enter the side of square :");
+  scanf("%d",&side);
+  printf("Area of square =%d",side*side);
+  getch()
+}
