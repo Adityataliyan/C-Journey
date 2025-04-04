604956bb0e980eea158181f53ebaaba137b56d29
Aditya Taliyan <adityataliyan123@gmail.com>
2025-04-04 15:18:19 +0530

    Create Enum.c
    In this program i use num data type

---
+++ Basic/Enum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+enum week {Mon,Tues, Wed, Thu,Fri,Sat,Sun};
+void main()
+{
+  clrscr();
+enum week today = Wed;
+printf("The week associate is %d",today);
+getch();
+}
